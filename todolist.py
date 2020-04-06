943220831264ad581f3dd22d06559294f0abe99c
Jeanne Amosse <jeanneamosse57@gmail.com>
2020-04-06 15:43:18 +0100

    todo list working for add delete and alter

---
+++ todolist.py
@@ -0,0 +1,41 @@
+from tabulate import tabulate
+import os
+
+items = []
+
+def draw_table(items):
+    content = [[i, elem] for i,elem in enumerate(items)]
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(tabulate(content, headers=["id", "todo list"]))
+    
+
+draw_table(items)
+
+
+while (True):
+    print("1) Add an item    2) Delete item   3) Modify")
+    choice = int(input())
+    
+    if (choice == 1):
+        print("What task should I add, Sun King?")
+        text = input()
+        items.append(text)
+        
+    if (choice == 2):
+        print("Which item do you want me to delete, Master?")
+        id = int(input())
+        del items[id]
+        
+
+    if (choice == 3):
+        print("Which task should I modify, Human-God?")
+        id = int(input())
+        print("Please enter new task")
+        new = input()
+        items[id]= new
+
+    draw_table(items)
+
+
+
+    
